500ed4a059183a4463a555d2f947162a66996bf2
Cyber kid <nahashonmash82@gmail.com>
2025-07-06 17:45:56 +0300

    remove hard-coded MongoDB URI, added .env, cleaned secrets

--- jitume-backend/server.js
+++ jitume-backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
 import Student from "./models/Student.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://Shon:Shon%40254@cluster0.tmxpvhi.mongodb.net/jitume?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("Could not connect to MongoDB:", err));
 
